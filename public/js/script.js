7dcc5e9ec4301fc6b64373fa09c9f51ffe749e55
Sunil More <sunilmore690@gmail.com>
2017-05-16 18:12:02 +0530

    show size in human readable format

--- public/js/script.js
+++ public/js/script.js
@@ -66,6 +66,22 @@ Handlebars.registerHelper('fileIcon',function(target){
     
   return 'fa-file-'+extension+'-o';
 })
+Handlebars.registerHelper('humanFileSize',function (bytes) {
+    var si = true;
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+})
 var BaseModel = Backbone.Model.extend({});
 
 var FtpConnect = Backbone.Model.extend({
